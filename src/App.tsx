922509d660f8ba4c152b747a6b88f4c7d18285b7
Robert <oratorio@udel.edu>
2023-04-12 14:15:38 -0400

    Added cart & styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
+            <div>
+                <h2 className="CartTitle">Customer&apos;s Cart</h2>
+                <Container className="Cart"></Container>
+                <p className="CartText">Total Price: </p>
+                <p className="CartText">Quantity: </p>
+            </div>
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
