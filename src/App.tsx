c085a99e40d77ff257d6959186699e34669d89e6
Michael Bocelli <mbocelli@udel.edu>
2023-04-13 19:46:45 -0400

    Removed {useState} import statement from App.tsx, so that website deploys

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { Container } from "react-bootstrap";
 import { CustomerCart } from "./components/CustomerCart";
