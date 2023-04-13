a5c7527603aa6ddf89947741b5192a168ec63d49
Sharanjit Singh <sharanjitsingh@wifi-roaming-128-4-88-141.wifi.udel.edu>
2023-04-13 16:00:08 -0400

    Added item 1

---
+++ src/Interface/food.ts
@@ -0,0 +1,13 @@
+export interface Food {
+    name: string;
+    description: string;
+    image: string;
+    price: float;
+    rating: number;
+    calories: number;
+    ingredients: string[];
+};
+    export constant foodList: Food[] = {
+        
+
+    }
