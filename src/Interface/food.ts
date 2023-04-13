3da99db17c454875db55e29f8768b8f188b2e66c
Sharanjit Singh <sharanjitsingh@wifi-roaming-128-4-88-141.wifi.udel.edu>
2023-04-13 16:02:51 -0400

    2nd food item

--- src/Interface/food.ts
+++ src/Interface/food.ts
@@ -2,12 +2,32 @@ export interface Food {
     name: string;
     description: string;
     image: string;
-    price: float;
+    price: number;
     rating: number;
     calories: number;
     ingredients: string[];
 };
-    export constant foodList: Food[] = {
-        
 
+
+export const foodList: Food[] = [
+    //1
+    {
+        name: "Apples",
+        description: "All organic Non-GMO Apples",
+        image: "idk",
+        price: 2.2,
+        rating: 9,
+        calories: 95,
+        ingredients: ["Apples"]
+    },
+    //2
+    {
+        name: "Whole Wheat Bread",
+        description: "Freshly Made Farmer Market Bread",
+        image: "idk",
+        price: 5.0,
+        rating: 7,
+        calories: 92,
+        ingredients: ["Water", "Flour", "Salt", "egg", "Yeast"]
     }
+];
