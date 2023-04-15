f047c9dc2b708ac258327ad12a315ec658b493be
Sharanjit Singh <sharanjitsingh@sharanjits-mbp.lan>
2023-04-15 14:10:15 -0400

    Added 10 additional food objects

--- src/Interface/food.ts
+++ src/Interface/food.ts
@@ -140,6 +140,107 @@ export const foodList: Food[] = [
             "Baking Powder",
             "Whole Milk"
         ]
-        //11
+        // Robert did 11-20
+    },
+    //21
+    {
+        name: "Peanut Chunk Chocolate",
+        description: "Savory taste of Peanuts with chocolate",
+        image: "idk",
+        price: 4.1,
+        rating: 8,
+        calories: 200,
+        ingredients: ["Sugar, Flour, Peanuts, cocoa "]
+    },
+    //22
+    {
+        name: "Potato Chips",
+        description: "Crisy Chips",
+        image: "idk",
+        price: 4.75,
+        rating: 2,
+        calories: 60,
+        ingredients: ["Potato, Salt"]
+    },
+    //23
+    {
+        name: "Oreo's",
+        description: "Creamy cookies",
+        image: "idk",
+        price: 6.12,
+        rating: 8,
+        calories: 75,
+        ingredients: ["Sugar, Cream filling, Chocolate"]
+    },
+    //24
+    {
+        name: "FairLife Protien, Chocolate",
+        description: "30 grams of pure isloate Protien per bottle. 18 Bottles",
+        image: "idk",
+        price: 30.5,
+        rating: 8,
+        calories: 150,
+        ingredients: ["Whey Protien, Chocolate, Milk"]
+    },
+    //25
+    //Make category vegan?
+    {
+        name: "Pea Protien",
+        description: "50 grams of pure isloate Protien per Scoop",
+        image: "idk",
+        price: 55.8,
+        rating: 9,
+        calories: 200,
+        ingredients: ["Pea Whey Protien, Chocolate, Milk"]
+    },
+    //26
+    {
+        name: "Rice Cakes",
+        description: "Crispy Rice Cakes with hint of honey",
+        image: "idk",
+        price: 12.25,
+        rating: 6,
+        calories: 80,
+        ingredients: ["Rice, Honey, Salt"]
+    },
+    //27
+    {
+        name: "FairLife Protien, Chocolate",
+        description: "30 grams of pure isloate Protien",
+        image: "idk",
+        price: 30.5,
+        rating: 8,
+        calories: 150,
+        ingredients: ["Whey Protien, Chocolate, Milk"]
+    },
+    //28
+    {
+        name: "Olive Oil",
+        description: "Fresh cooking olive oil",
+        image: "idk",
+        price: 7.85,
+        rating: 8,
+        calories: 150,
+        ingredients: ["Whey Protien, Chocolate, Milk"]
+    },
+    //29
+    {
+        name: "Granola Bars",
+        description: "Crispy valley oats Granola",
+        image: "idk",
+        price: 10.3,
+        rating: 10,
+        calories: 250,
+        ingredients: ["Granola, Sugar, Honey"]
+    },
+    //30
+    {
+        name: "PopCorn",
+        description: "Buttery PopCorn",
+        image: "idk",
+        price: 30.5,
+        rating: 8,
+        calories: 150,
+        ingredients: ["Corn, Butter, preservatives"]
     }
 ];
