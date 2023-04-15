eb36f702cae5d3a04d2b50bbec598bf25cc7614d
Michael Bocelli <mbocelli@udel.edu>
2023-04-15 12:43:14 -0400

    Added state for the list so that super user may add and remove FoodItems later

--- src/components/CentralList.tsx
+++ src/components/CentralList.tsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Food, FOOD_LIST } from "../interfaces/food";
 import { FoodItem } from "./FoodItem";
 
 export function CentralList(): JSX.Element {
+    const [centralList] = useState<Food[]>(FOOD_LIST);
     return (
         <div>
-            {FOOD_LIST.map((food: Food) => (
+            {centralList.map((food: Food) => (
                 <FoodItem
                     key={food.name}
                     name={food.name}
-                    description={""}
-                    image={""}
-                    price={0}
-                    calories={0}
-                    ingredients={[]}
+                    description={food.description}
+                    image={food.image}
+                    price={food.price}
+                    calories={food.calories}
+                    ingredients={food.ingredients}
                 ></FoodItem>
             ))}
         </div>
