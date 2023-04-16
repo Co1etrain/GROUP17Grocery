4d9c52cde9cb03be67259effc210bd481ad1c6c8
Andrew Kallai <andrewka@udel.edu>
2023-04-15 21:54:45 -0400

    Added a form component to change the global user state, included limited functionality for displaying multiple users per selected state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Container } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 import { CustomerCart } from "./components/CustomerCart";
 import { CentralList } from "./components/CentralList";
-type userType = "owner" | "employee" | "customer";
+// eslint-disable-next-line
+import { Users, userLookup } from "./interfaces/record";
 
 function App(): JSX.Element {
     // eslint-disable-next-line
-    const [currentUser, setUser] = useState<userType>("owner");
+    const [currentUser, setUser] = useState<Users["person"]>("owner");
+
+    function updateUser(event: React.ChangeEvent<HTMLSelectElement>) {
+        const toUsersType: Users["person"] = event.target
+            .value as Users["person"];
+        setUser(toUsersType);
+    }
+
     return (
         <div className="App">
+            <div>
+                <Form.Group controlId="userSelected">
+                    <Form.Label>Select User:</Form.Label>
+                    <Form.Select value={currentUser} onChange={updateUser}>
+                        <option value="owner">
+                            owner{userLookup["owner"]}
+                        </option>
+                        <option value="employee">
+                            employee{userLookup["employee"]}
+                        </option>
+                        <option value="customer">
+                            customer{userLookup["customer"]}
+                        </option>
+                    </Form.Select>
+                </Form.Group>
+            </div>
             <div>
                 <h2 className="CartTitle">Customer&apos;s Cart</h2>
                 <Container className="Cart">
