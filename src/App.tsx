b59c118b3aafea88f941090e7b28ebdb812ea290
Michael Bocelli <mbocelli@udel.edu>
2023-04-18 12:19:15 -0400

    Imported react-dnd to App.tsx, and added DnDProvider wrapper to entire App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Container, Form } from "react-bootstrap";
 import { CustomerCart } from "./components/CustomerCart";
 import { CentralList } from "./components/CentralList";
 import { Users, userLookup } from "./interfaces/record";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App(): JSX.Element {
     const [currentUser, setUser] = useState<Users["person"]>("owner");
@@ -15,45 +17,47 @@ function App(): JSX.Element {
     }
 
     return (
-        <div className="App">
-            <p>Michael Bocelli</p>
-            <p>Robert Oratorio</p>
-            <p>Sharanjit Singh</p>
-            <p>Cole McCaleb</p>
-            <p>Andrew Kallai</p>
-            <div>
-                <Form.Group controlId="userSelected">
-                    <Form.Label>Select User:</Form.Label>
-                    <Form.Select value={currentUser} onChange={updateUser}>
-                        <option value="owner">
-                            owner{userLookup["owner"]}
-                        </option>
-                        <option value="employee">
-                            employee{userLookup["employee"]}
-                        </option>
-                        <option value="customer">
-                            customer{userLookup["customer"]}
-                        </option>
-                    </Form.Select>
-                </Form.Group>
+        <DndProvider backend={HTML5Backend}>
+            <div className="App">
+                <p>Michael Bocelli</p>
+                <p>Robert Oratorio</p>
+                <p>Sharanjit Singh</p>
+                <p>Cole McCaleb</p>
+                <p>Andrew Kallai</p>
+                <div>
+                    <Form.Group controlId="userSelected">
+                        <Form.Label>Select User:</Form.Label>
+                        <Form.Select value={currentUser} onChange={updateUser}>
+                            <option value="owner">
+                                owner{userLookup["owner"]}
+                            </option>
+                            <option value="employee">
+                                employee{userLookup["employee"]}
+                            </option>
+                            <option value="customer">
+                                customer{userLookup["customer"]}
+                            </option>
+                        </Form.Select>
+                    </Form.Group>
+                </div>
+                <div>
+                    <h2 className="CartTitle">Customer&apos;s Cart</h2>
+                    <Container className="Cart">
+                        <CustomerCart></CustomerCart>
+                    </Container>
+                    <p className="CartText">Total Price: </p>
+                    <p className="CartText">Quantity: </p>
+                </div>
+                <CentralList></CentralList>
+                <header className="App-header">
+                    UD CISC275 with React Hooks and TypeScript
+                </header>
+                <p>
+                    Edit <code>src/App.tsx</code> and save. This page will
+                    automatically reload.
+                </p>
             </div>
-            <div>
-                <h2 className="CartTitle">Customer&apos;s Cart</h2>
-                <Container className="Cart">
-                    <CustomerCart></CustomerCart>
-                </Container>
-                <p className="CartText">Total Price: </p>
-                <p className="CartText">Quantity: </p>
-            </div>
-            <CentralList></CentralList>
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-        </div>
+        </DndProvider>
     );
 }
 
