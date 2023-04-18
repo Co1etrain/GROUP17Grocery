bb239766bb0089210d665128bf08a1794f012af2
Michael Bocelli <mbocelli@udel.edu>
2023-04-18 13:01:43 -0400

    Made FoodItem render the test image

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Food } from "../interfaces/food";
+import test from "../test_image/test.jpeg";
 
 export function FoodItem({
     name,
@@ -13,10 +14,11 @@ export function FoodItem({
     const [rating] = useState<number>(0);
     return (
         <div>
-            {name}
+            <img src={test} width="100px"></img>
             <div hidden={true}>
-                {description}
+                {name}
                 {image}
+                {description}
                 {price}
                 {calories}
                 {ingredients}
