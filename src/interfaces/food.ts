491bf6d1cb98d056211eb597ccc33ddbe71444ce
Michael Bocelli <mbocelli@udel.edu>
2023-04-18 12:25:47 -0400

    Added relative path link to test.jpeg for each food object in FOOD_LIST

--- src/interfaces/food.ts
+++ src/interfaces/food.ts
@@ -13,7 +13,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Apples",
         description: "All organic Non-GMO Apples",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 2.2,
         calories: 95,
         ingredients: ["Apples"],
@@ -23,7 +23,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Whole Wheat Bread",
         description: "Freshly Made Farmer Market Bread",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 5.0,
         calories: 92,
         ingredients: ["Water", "Flour", "Salt", "egg", "Yeast"],
@@ -33,7 +33,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Peanut Butter",
         description: "Easy Spread PeanutButter",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 6.25,
         calories: 80,
         ingredients: ["Peanut", "Water"],
@@ -43,7 +43,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Chocolate Chip Cookies",
         description: "Freshly made Nestle Chip Cookies",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 7.5,
         calories: 50,
         ingredients: [
@@ -59,7 +59,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Banana",
         description: "Fresh Bananas",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 1.5,
         calories: 105,
         ingredients: ["Banana"],
@@ -69,7 +69,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Hummus",
         description: "Spicy Hummus",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 4.35,
         calories: 125,
         ingredients: [
@@ -91,7 +91,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "BlueBerry Yogurt",
         description: "",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 2.5,
         calories: 105,
         ingredients: [
@@ -108,7 +108,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Broccoli",
         description: "Frozen Pack of Broccoli",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 3.75,
         calories: 100,
         ingredients: ["Banana"],
@@ -118,7 +118,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Lemon Lime Soda",
         description: "Savory taste of the Lemon",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 3.75,
         calories: 145,
         ingredients: ["Lemon", "Lime", "Club Soda", "Sugar"],
@@ -128,7 +128,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Slighty Salted Cracker",
         description: "Crisp Salted Cracker, 20 Crackers",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 5.65,
         calories: 105,
         ingredients: [
@@ -146,7 +146,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Peanut Chunk Chocolate",
         description: "Savory taste of Peanuts with chocolate",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 4.1,
         calories: 200,
         ingredients: ["Sugar, Flour, Peanuts, cocoa "],
@@ -156,7 +156,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Potato Chips",
         description: "Crisy Chips",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 4.75,
         calories: 60,
         ingredients: ["Potato, Salt"],
@@ -166,7 +166,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Oreo's",
         description: "Creamy cookies",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 6.12,
         calories: 75,
         ingredients: ["Sugar, Cream filling, Chocolate"],
@@ -176,7 +176,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "FairLife Protien, Chocolate",
         description: "30 grams of pure isloate Protien per bottle. 18 Bottles",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 30.5,
         calories: 150,
         ingredients: ["Whey Protien, Chocolate, Milk"],
@@ -187,7 +187,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Pea Protien",
         description: "50 grams of pure isloate Protien per Scoop",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 55.8,
         calories: 200,
         ingredients: ["Pea Whey Protien, Chocolate, Milk"],
@@ -197,7 +197,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Rice Cakes",
         description: "Crispy Rice Cakes with hint of honey",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 12.25,
         calories: 80,
         ingredients: ["Rice, Honey, Salt"],
@@ -207,7 +207,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "FairLife Protien, Chocolate",
         description: "30 grams of pure isloate Protien",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 30.5,
         calories: 150,
         ingredients: ["Whey Protien, Chocolate, Milk"],
@@ -217,7 +217,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Olive Oil",
         description: "Fresh cooking olive oil",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 7.85,
         calories: 150,
         ingredients: ["Whey Protien, Chocolate, Milk"],
@@ -227,7 +227,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Granola Bars",
         description: "Crispy valley oats Granola",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 10.3,
         calories: 250,
         ingredients: ["Granola, Sugar, Honey"],
@@ -237,7 +237,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "PopCorn",
         description: "Buttery PopCorn",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 30.5,
         calories: 150,
         ingredients: ["Corn, Butter, preservatives"],
@@ -247,7 +247,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Eggs",
         description: "Delicious chicken eggs",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 5.5,
         calories: 120,
         ingredients: ["Eggs"],
@@ -257,7 +257,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Tomatos",
         description: "Delicious sweet red tomatos",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 5.45,
         calories: 100,
         ingredients: ["Tomato"],
@@ -267,7 +267,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Lemon",
         description: "Delicious",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 5.45,
         calories: 20,
         ingredients: ["Lemon"],
@@ -277,7 +277,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Celery",
         description: "Nice low-calorie snack",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 2.15,
         calories: 10,
         ingredients: ["Celery"],
@@ -287,7 +287,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Strawberries",
         description: "Sweet and juicy snack",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 3.45,
         calories: 100,
         ingredients: ["Strawberry"],
@@ -297,7 +297,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Ribeye Steak",
         description: "High-quality savory cut of meat",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 50.95,
         calories: 500,
         ingredients: ["Beef, Salt, Pepper"],
@@ -307,7 +307,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Pizza",
         description: "Delicious frozen pizza",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 7.25,
         calories: 1000,
         ingredients: ["Bread, Tomato Sauce, Cheese, Pepperoni"],
@@ -317,7 +317,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Black Beans",
         description: "Popular Latin American cuisine",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 3.95,
         calories: 200,
         ingredients: ["Beans, Water, Salt"],
@@ -327,7 +327,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Ground Beef",
         description: "Savory dish, 100% beef",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 17.15,
         calories: 300,
         ingredients: ["Beef, Salt, Pepper"],
@@ -337,7 +337,7 @@ export const FOOD_LIST: Food[] = [
     {
         name: "Cheese",
         description: "Tasty cheese",
-        image: "idk",
+        image: "../test_image/test.jpeg",
         price: 5.85,
         calories: 275,
         ingredients: ["Milk, Salt, Cheese"],
