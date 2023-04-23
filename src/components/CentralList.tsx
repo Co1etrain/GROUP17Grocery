267eafc84e43cb8b17fb10b0d9a732df2d75c6ca
Michael Bocelli <mbocelli@udel.edu>
2023-04-23 12:12:53 -0400

    Added quick styling to central list

--- src/components/CentralList.tsx
+++ src/components/CentralList.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Food, FOOD_LIST } from "../interfaces/food";
 import { FoodItem } from "./FoodItem";
+import "../App.css";
 
 export function CentralList(): JSX.Element {
     const [centralList] = useState<Food[]>(FOOD_LIST);
     return (
-        <div>
+        <div className="CentralList">
             {centralList.map((food: Food) => {
                 return (
                     <FoodItem
