0f62be5d44980d9e029d7820834e688fe4c1019d
Andrew Kallai <andrewka@udel.edu>
2023-04-24 13:18:14 -0400

    Created CustomersForm document

---
+++ src/components/CustomersForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+
+export function DisplayCustomerNames(): JSX.Element {
+    return <div></div>;
+    }
