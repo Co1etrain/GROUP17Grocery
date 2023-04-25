a51ca0a671e4d56d335fb1b44ffc83615b7741f0
Michael Bocelli <mbocelli@udel.edu>
2023-04-25 16:18:38 -0400

    Added total cart price and fixed decimal places to .00 for each fooditem and total price

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -39,7 +39,7 @@ export function FoodItem({
             >
                 <p>
                     <strong>
-                        {name} - ${price}
+                        {name} - ${price.toFixed(2)}
                     </strong>
                     <br></br>
                     {description}
