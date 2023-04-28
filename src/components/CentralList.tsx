fb2f010318cec86ae205ff43827cf1ecfc226ba7
Sharanjit Singh <sharanjitsingh@wifi-roaming-128-4-120-234.wifi.udel.edu>
2023-04-28 17:08:19 -0400

    Made Delete button for central list

--- src/components/CentralList.tsx
+++ src/components/CentralList.tsx
@@ -1,32 +1,52 @@
-import React from "react";
+/* eslint-disable no-extra-parens */
+import React, { useState } from "react";
 import { Food } from "../interfaces/food";
 import { FoodItem } from "./FoodItem";
 import "../App.css";
+import { Users } from "../interfaces/record";
 //create a button given a name/ form, name of food and create a new central list without the deleted
 
 export function CentralList({
     foodList,
-    onFoodUpdate
+    onFoodUpdate,
+    currentUser
 }: {
     foodList: Food[];
     onFoodUpdate: (updatedFood: Food) => void;
+    currentUser: Users["person"];
 }): JSX.Element {
+    const [centralList, setCentralList] = useState<Food[]>(foodList);
+
+    function deleteFood(name: string) {
+        const newCentralList = centralList.filter((food) => food.name !== name);
+        setCentralList(newCentralList);
+    }
+
     return (
         <div className="CentralList" id="middle">
-            {foodList.map((food: Food) => {
+            {centralList.map((food: Food) => {
                 return (
-                    <FoodItem
-                        id={food.id}
-                        key={food.id}
-                        name={food.name}
-                        description={food.description}
-                        image={food.image}
-                        price={food.price}
-                        calories={food.calories}
-                        ingredients={food.ingredients}
-                        category={food.category}
-                        onFoodUpdate={onFoodUpdate}
-                    ></FoodItem>
+                    <div key={food.id}>
+                        <FoodItem
+                            id={food.id}
+                            key={food.id}
+                            name={food.name}
+                            description={food.description}
+                            image={food.image}
+                            price={food.price}
+                            calories={food.calories}
+                            ingredients={food.ingredients}
+                            category={food.category}
+                            onFoodUpdate={onFoodUpdate}
+                        ></FoodItem>
+                        {currentUser == "owner" ? (
+                            <button onClick={() => deleteFood(food.name)}>
+                                Delete
+                            </button>
+                        ) : (
+                            <div> Invalid user </div>
+                        )}
+                    </div>
                 );
             })}
         </div>
