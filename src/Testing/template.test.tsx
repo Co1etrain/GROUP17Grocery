e1c5d38e90100cc59214cfc9a7d6c2d281d261ca
Andrew Kallai <andrewka@udel.edu>
2023-05-11 16:09:24 -0400

    Made a template testing file for the central list and added other testing files

---
+++ src/Testing/template.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { CentralList } from "../components/CentralList";
+import { Food, FOOD_LIST } from "../interfaces/food";
+import { User } from "../interfaces/user";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+describe("CentralList", () => {
+
+    const testUser: User = {
+        name: "John Doe",
+        foodList: [],
+        role: "customer"
+    };
+
+    test("renders CentralList component", () => {
+        render(
+            <DndProvider backend={HTML5Backend}>
+                <CentralList
+                    centralList={FOOD_LIST}
+                    onFoodUpdate={(updatedFood: Food) => {}}
+                    currentUser={testUser}
+                />
+            </DndProvider>
+        );
+    });
+});
